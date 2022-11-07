8f28eb0a085ebb6f3bd341a3611eb3ac6d356589
NyiteX <kitheus09@gmail.com>
2022-11-07 23:36:49 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Basic_Window
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.X_Button = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button_next = new System.Windows.Forms.Button();
+            this.button_prev = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // X_Button
+            // 
+            this.X_Button.AutoSize = true;
+            this.X_Button.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.X_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.X_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.X_Button.Location = new System.Drawing.Point(479, 9);
+            this.X_Button.Name = "X_Button";
+            this.X_Button.Size = new System.Drawing.Size(23, 22);
+            this.X_Button.TabIndex = 0;
+            this.X_Button.Text = "X";
+            this.X_Button.Click += new System.EventHandler(this.X_Button_Click);
+            this.X_Button.MouseEnter += new System.EventHandler(this.X_Button_MouseEnter);
+            this.X_Button.MouseLeave += new System.EventHandler(this.X_Button_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(93, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Red 220";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(93, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Green 220";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(93, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Blue 220";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(168, 107);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(223, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 220;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(168, 158);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(223, 45);
+            this.trackBar2.TabIndex = 6;
+            this.trackBar2.Value = 220;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(168, 213);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(223, 45);
+            this.trackBar3.TabIndex = 7;
+            this.trackBar3.Value = 220;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(211, 29);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(86, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // button_next
+            // 
+            this.button_next.Location = new System.Drawing.Point(414, 295);
+            this.button_next.Name = "button_next";
+            this.button_next.Size = new System.Drawing.Size(75, 23);
+            this.button_next.TabIndex = 9;
+            this.button_next.Text = "Next";
+            this.button_next.UseVisualStyleBackColor = true;
+            this.button_next.Click += new System.EventHandler(this.button_next_Click);
+            // 
+            // button_prev
+            // 
+            this.button_prev.Location = new System.Drawing.Point(66, 295);
+            this.button_prev.Name = "button_prev";
+            this.button_prev.Size = new System.Drawing.Size(75, 23);
+            this.button_prev.TabIndex = 10;
+            this.button_prev.Text = "Prev";
+            this.button_prev.UseVisualStyleBackColor = true;
+            this.button_prev.Visible = false;
+            this.button_prev.Click += new System.EventHandler(this.button_prev_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(162, 264);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(229, 68);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Visible = false;
+            this.textBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseMove);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(141, 184);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(270, 23);
+            this.progressBar1.TabIndex = 12;
+            this.progressBar1.Visible = false;
+            // 
+            // Basic_Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 377);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_prev);
+            this.Controls.Add(this.button_next);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.X_Button);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Basic_Window";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Basic_Window_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Basic_Window_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label X_Button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button_next;
+        private System.Windows.Forms.Button button_prev;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp5
+{
+    public partial class Basic_Window : Form
+    {
+        Point p;
+
+        public Basic_Window()
+        {
+            InitializeComponent();
+
+            timer1.Start();
+            timer1.Tick += new EventHandler(timer1_Tick);
+
+            BackColor = Color.FromArgb(220,220,220);
+
+            textBox1.MouseWheel += MyMouseWheel;
+            //MouseWheel += textBox1_MouseMove;
+        }
+
+        //Basic_Window
+        private void Basic_Window_MouseDown(object sender, MouseEventArgs e)
+        {
+            p = new Point(e.X, e.Y);
+        }
+        private void Basic_Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Left += e.X - p.X;
+                Top += e.Y - p.Y;
+            }
+        }
+
+        //Basic_buttons
+        private void X_Button_MouseEnter(object sender, EventArgs e)
+        {
+            X_Button.BorderStyle = BorderStyle.Fixed3D;
+            X_Button.BackColor = Color.Thistle;
+        }
+        private void X_Button_MouseLeave(object sender, EventArgs e)
+        {
+            X_Button.BorderStyle = BorderStyle.FixedSingle;
+            X_Button.BackColor = Color.WhiteSmoke;
+        }
+        private void X_Button_Click(object sender, EventArgs e)
+        {
+            Close();
+        }       
+
+        private void Read_File()
+        {
+            try
+            {
+                if(File.Exists("test.txt"))
+                using (StreamReader F = new StreamReader("test.txt"))
+                {
+                    textBox1.Text = F.ReadToEnd();
+                }
+            }
+            catch(Exception E) { MessageBox.Show(E.Message); }
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int r = trackBar1.Value;
+            int g = trackBar2.Value;
+            int b = trackBar3.Value;
+            BackColor = Color.FromArgb(r,g,b);
+            label1.Text = "Red " + r;
+        }
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            int r = trackBar1.Value;
+            int g = trackBar2.Value;
+            int b = trackBar3.Value;
+            BackColor = Color.FromArgb(r, g, b);
+            label2.Text = "Green " + g;
+        }
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            int r = trackBar1.Value;
+            int g = trackBar2.Value;
+            int b = trackBar3.Value;
+            BackColor = Color.FromArgb(r, g, b);
+            label3.Text = "Blue " + b;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Now;
+        }
+        private void button_next_Click(object sender, EventArgs e)
+        {
+            button_next.Visible = false;
+            button_prev.Visible = true;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            trackBar1.Visible = false;
+            trackBar2.Visible = false;
+            trackBar3.Visible = false;
+            textBox1.Visible = true;
+            progressBar1.Visible = true;
+            Read_File();
+        }
+        private void button_prev_Click(object sender, EventArgs e)
+        {
+            button_prev.Visible = false;
+            button_next.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            trackBar1.Visible = true;
+            trackBar2.Visible = true;
+            trackBar3.Visible = true;
+            textBox1.Visible = false;
+            progressBar1.Visible = false;
+        }
+        //Mouse_click progressBar
+        private void textBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //progressBar1.Maximum = textBox1.Lines.Length - 5;
+            //progressBar1.Value = textBox1.GetFirstCharIndexOfCurrentLine() / textBox1.Lines.Length;                   
+        }
+        //Scroll!!11 progressBar
+        private void MyMouseWheel(object sender, MouseEventArgs e)
+        {
+                if (e.Delta > 0 && textBox1.Lines.Length > 5)
+                {
+                    progressBar1.Maximum = textBox1.Lines.Length-2;
+                    if (progressBar1.Value + e.Delta / 40 * -1 >= progressBar1.Minimum)
+                        progressBar1.Value += e.Delta / 40 * -1;
+                }
+                else if (e.Delta < 0 && textBox1.Lines.Length > 5)
+                {
+                    progressBar1.Maximum = textBox1.Lines.Length-2;
+                    if (progressBar1.Value + e.Delta / 40 * -1 <= progressBar1.Maximum)
+                        progressBar1.Value += e.Delta / 40 * -1;
+                }
+        }
+    }
+}
